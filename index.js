ea5fe9eed5c18abde1f3ef12379408ae3c06ab4f
Alex <alexferreiraa20@gmail.com>
2023-01-26 16:47:20 -0300

    Exercicios Package-json check

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("aplicativo iniciado");
+
+const value = process.argv[2]
+
+if(!value){
+    console.log("digite um argumentozinho");
+}else{
+    const result = "bananinha"
+    console.log(result);
+}
